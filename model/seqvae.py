ce260398f43ca0e3484f0f23eb10ce81b4e856cd
Eric Bae <eric.kh.bae@gmail.com>
2024-12-12 22:58:16 -0800

    updated seqvae

--- model/seqvae.py
+++ model/seqvae.py
@@ -1,10 +1,13 @@
 import numpy as np
 import torch as t
 import torch.nn as nn
+import torch.nn.functional as F
 
 from .embedding import Embedding
 from .encoder import Encoder
 from .decoder import Decoder
+
+from utils.functions import kld_coef
 
 class SeqVAE(nn.Module):
     def __init__(self, params):
@@ -18,18 +21,12 @@ class SeqVAE(nn.Module):
         self.decoder = Decoder(self.params)
         
         # formulas to use in forward pass
-        self.context_to_mu = nn.Linear(self.params.encoder_rnn_size * 2, self.params.latent_variable_size)
+        self.context_to_mu = nn.Linear(self.params.encoder_rnn_size * 2, self.params.latent_variable_size) # TODO: what is latent_variable_size?
         self.context_to_logvar = nn.Linear(self.params.encoder_rnn_size * 2, self.params.latent_variable_size)
         
     
-    def forward(self, drop_prob,
-                encoder_word_input=None, encoder_character_input=None,
-                decoder_word_input=None, decoder_character_input=None,
-                z=None, initial_state=None):
+    def forward(self, drop_prob, z=None, initial_state=None):
         """
-        :param encoder_word_input: An tensor with shape of [batch_size, seq_len] of Long type
-        :param encoder_character_input: An tensor with shape of [batch_size, seq_len, max_word_len] of Long type
-        :param decoder_word_input: An tensor with shape of [batch_size, max_seq_len + 1] of Long type
         :param initial_state: initial state of decoder rnn in order to perform sampling
 
         :param drop_prob: probability of an element of decoder input to be zeroed in sense of dropout
@@ -39,17 +36,9 @@ class SeqVAE(nn.Module):
         :return: unnormalized logits of sentence words distribution probabilities
                     with shape of [batch_size, seq_len, word_vocab_size]
                  final rnn state with shape of [num_layers, batch_size, decoder_rnn_size]
+                 
+        return: distribution probabilities with shape (n, t, m)
         """
-
-        assert parameters_allocation_check(self), \
-            'Invalid CUDA options. Parameters should be allocated in the same memory'
-        use_cuda = self.embedding.word_embed.weight.is_cuda
-
-        assert z is None and fold(lambda acc, parameter: acc and parameter is not None,
-                                  [encoder_word_input, encoder_character_input, decoder_word_input],
-                                  True) \
-            or (z is not None and decoder_word_input is not None), \
-            "Invalid input. If z is None then encoder and decoder inputs should be passed as arguments"
 
         if z is None:
             ''' Get context from encoder and sample z ~ N(mu, std)
@@ -63,13 +52,16 @@ class SeqVAE(nn.Module):
             mu = self.context_to_mu(context)
             logvar = self.context_to_logvar(context)
             std = t.exp(0.5 * logvar)
-
-            z = Variable(t.randn([batch_size, self.params.latent_variable_size]))
-            if use_cuda:
-                z = z.cuda()
-
+            
+            # check if possible on gpu
+            z = t.randn([batch_size, self.params.latent_variable_size])
+            device = t.device("cuda" if t.cuda.is_available() else "cpu")
+            z = z.to(device)
+            
+            # reparameterization trick
             z = z * std + mu
 
+            # KL divergence loss
             kld = (-0.5 * t.sum(logvar - t.pow(mu, 2) - t.exp(logvar) + 1, 1)).mean().squeeze()
         else:
             kld = None
@@ -87,7 +79,7 @@ class SeqVAE(nn.Module):
     def trainer(self, optimizer, batch_loader):
         def train(i, batch_size, use_cuda, dropout):
             input = batch_loader.next_batch(batch_size, 'train')
-            input = [Variable(t.from_numpy(var)) for var in input]
+            input = [t.from_numpy(np.array(var)) for var in input]
             input = [var.long() for var in input]
             input = [var.cuda() if use_cuda else var for var in input]
 
@@ -115,7 +107,7 @@ class SeqVAE(nn.Module):
     def validater(self, batch_loader):
         def validate(batch_size, use_cuda):
             input = batch_loader.next_batch(batch_size, 'valid')
-            input = [Variable(t.from_numpy(var)) for var in input]
+            input = [t.from_numpy(var) for var in input]
             input = [var.long() for var in input]
             input = [var.cuda() if use_cuda else var for var in input]
 
@@ -136,14 +128,15 @@ class SeqVAE(nn.Module):
         return validate
 
     def sample(self, batch_loader, seq_len, seed, use_cuda):
-        seed = Variable(t.from_numpy(seed).float())
+        seed = t.from_numpy(seed).float()
         if use_cuda:
             seed = seed.cuda()
 
         decoder_word_input_np, decoder_character_input_np = batch_loader.go_input(1)
 
-        decoder_word_input = Variable(t.from_numpy(decoder_word_input_np).long())
-        decoder_character_input = Variable(t.from_numpy(decoder_character_input_np).long())
+        decoder_word_input = t.from_numpy(decoder_word_input_np).long()
+        decoder_character_input = t.from_numpy(decoder_character_input_np).long()
+
 
         if use_cuda:
             decoder_word_input, decoder_character_input = decoder_word_input.cuda(), decoder_character_input.cuda()
@@ -170,9 +163,9 @@ class SeqVAE(nn.Module):
             decoder_word_input_np = np.array([[batch_loader.word_to_idx[word]]])
             decoder_character_input_np = np.array([[batch_loader.encode_characters(word)]])
 
-            decoder_word_input = Variable(t.from_numpy(decoder_word_input_np).long())
-            decoder_character_input = Variable(t.from_numpy(decoder_character_input_np).long())
-
+            decoder_word_input = t.from_numpy(decoder_word_input_np).long()
+            decoder_character_input = t.from_numpy(decoder_character_input_np).long()
+            
             if use_cuda:
                 decoder_word_input, decoder_character_input = decoder_word_input.cuda(), decoder_character_input.cuda()
 
--- utils/functions.py
+++ utils/functions.py
@@ -1,1 +1,5 @@
 
+# trying this out
+def kld_coef(i):
+    import math
+    return (math.tanh((i - 3500)/1000) + 1)/2
