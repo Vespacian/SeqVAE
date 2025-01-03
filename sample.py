7a3ef022f08b578901cb91c2c2bf53f08acbb733
Michael <michael.mingdao.li@gmail.com>
2025-01-03 11:34:01 -0800

    Superior sampling code

--- sample.py
+++ sample.py
@@ -1,12 +1,13 @@
 import torch
 import matplotlib.pyplot as plt
+import math
 
 from model.seqvae import SeqVAE
 
 # Constants, modify for plotting
 MODEL_NAME = 'model_gaussian-model_20250102T213123.pth'
 MODEL_PATH = f'results/models/{MODEL_NAME}'
-NUM_SAMPLES = 4
+NUM_SAMPLES = 12
 ELEMENT_DIM = 2
 HIDDEN_DIM = 256
 LATENT_DIM = 1024
@@ -22,24 +23,32 @@ def plot_samples(model, num_samples=5):
     with torch.no_grad():
         sampled_sequences = model.sample(num_samples=num_samples, seq_length=sequence_length, device=DEVICE)
 
+    # Utility function for plotting subplot
+    def subplot_embedding(subplot, graph, title):
+        subplot.scatter(graph[:,0], graph[:,1], color='blue')
+        subplot.set_xticks([])
+        subplot.set_yticks([])
+        subplot.set_title(title)
+
     # Plot each sequence
     sampled_sequences_np = sampled_sequences.cpu().numpy()
-    fig, ax = plt.subplots(figsize=(8, 6))
-    for i, seq in enumerate(sampled_sequences_np):
-        x = seq[:, 0]
-        y = seq[:, 1]
-        ax.scatter(x, y, marker='o', label=f"Sample {i+1}")
+    WIDTH = 4
+    HEIGHT = math.ceil(num_samples / WIDTH)
 
-    # Label plot
-    ax.set_title(f"Sampled Sequences")
-    ax.grid(True)
-    ax.legend()
+    fig, axs = plt.subplots(HEIGHT, WIDTH, figsize=(WIDTH * 3, HEIGHT * 3))
+    plt.subplots_adjust(hspace=0.1)
+    plt.tight_layout(rect=[0.01, 0, 0.97, 0.98])
+
+    for i, sample in enumerate(sampled_sequences_np):
+        idx1 = i // WIDTH
+        idx2 = i % WIDTH
+        subplot_embedding(axs[idx1, idx2], sample, f"Sample {i+1}")
 
     plt.show()
 
 # Load model and plot
 model = SeqVAE(input_dim=ELEMENT_DIM, hidden_dim=HIDDEN_DIM, latent_dim=LATENT_DIM, seq_length=GRAPH_SIZE)
-model.load_state_dict(torch.load(MODEL_PATH))
+model.load_state_dict(torch.load(MODEL_PATH, weights_only=True))
 model.to(DEVICE)
 print("Model loaded from:", MODEL_PATH)
 
